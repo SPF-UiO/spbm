96abacc841e39fff0eaa4f58e2fea1c051f4f059
Aleksi Luukkonen <aleksiml@ifi.uio.no>
2015-02-20 01:09:31 +0000

    Fix errorneous STATIC_ROOT

--- spf_web/settings.py
+++ spf_web/settings.py
@@ -75,7 +75,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/aleksi/spf_web/static_files'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
