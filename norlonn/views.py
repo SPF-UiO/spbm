6c47a3ed1ebefe49a30fb3c200bc73397fc6151e
Aleksi Luukkonen <aleksiml@ifi.uio.no>
2015-03-03 14:40:32 +0100

    Group payments so that each person receives one line.

--- norlonn/views.py
+++ norlonn/views.py
@@ -7,15 +7,25 @@ from decimal import Decimal
 def get_report(request, date):
 	events = Event.objects.filter(processed=date)
 	linjer = []
+	personshift = {}
 
 	for e in events:
 		shift = Shift.objects.filter(event=e)
 		for s in shift:
-			hours = s.hours*Decimal("100")
-			hours = hours.quantize(Decimal("10"))
-			wage = s.wage*Decimal("100")
-			wage = wage.quantize(Decimal("10"))
-			linjer.append(";"+str(s.worker.norlonn_number)+";H1;"+str(hours)+";"+str(wage)+";;")
+			if s.worker.norlonn_number in personshift:
+				personshift[s.worker.norlonn_number].append(s)
+			else:
+				personshift[s.worker.norlonn_number] = [s, ]
+	print(str(personshift))
+	for (nl,ss) in personshift.items():
+		total = Decimal(0)
+		
+		for s in ss:
+			total += s.hours*s.wage
+	
+		total = total*Decimal("100")	
+		total = total.quantize(Decimal("10"))
+		linjer.append(";"+str(s.worker.norlonn_number)+";H1;;;"+str(total))
 
 	return HttpResponse("\n".join(linjer), content_type="text/plain")
 # Create your views here.
