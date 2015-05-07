85c0b50d8e4310a509149e166f9402e1fc1c187e
Aleksi Luukkonen <aleksiml@ifi.uio.no>
2015-05-07 01:21:05 +0000

    Add norlønn reporting

--- norlonn/views.py
+++ norlonn/views.py
@@ -1,22 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required,user_passes_test
 
 from events.models import Shift,Event
 from decimal import Decimal
+from norlonn.models import NorlonnReport
 
+@login_required
+def index(request):
+	reports = NorlonnReport.objects.all().order_by('-date')
+	errors = []
+
+	for s in Shift.objects.filter(norlonn_report__isnull=True, event__processed__isnull=False):
+		if s.worker.norlonn_number is None:
+			errors.append("Worker %s lacks norlonn number!" % s.worker)
+
+		if s.event.invoice.paid == False:
+			errors.append("Invoice not paid for worker %s" % s.worker)
+
+	return render(request, 'norlonn/index.jinja', { 'reports': reports, 'errors': errors })
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def generate_report(request):
+	if request.method != "POST":
+		return redirect(index)
+
+	errors = []
+	succ = []
+	shifts = Shift.objects.filter(norlonn_report__isnull=True, event__processed__isnull=False)
+
+	if not shifts.exists():
+		return HttpResponse("Nothing to generate!")
+
+	if NorlonnReport.objects.filter(date=timezone.now()).exists():
+		return HttpResponse("Already exists!")
+
+	report = NorlonnReport(date=timezone.now())
+	report.save()
+	
+	for s in shifts:
+		if s.worker.norlonn_number is None:
+			errors.append("ERR: %s - %s --- Lacks norlonn number" % (s.worker, s.event))
+			continue
+		
+		if s.event.invoice.paid == False:
+			errors.append("ERR: %s - %s --- Invoice not paid" % (s.worker, s.event))
+			continue
+
+		succ.append("OK: %s - %s" % (s.worker, s.event))
+		s.norlonn_report = report
+		s.save()
+
+	if len(succ) == 0:
+		report.delete()
+		return HttpResponse("Nothing to generate!");
+
+	return render(request, 'norlonn/report.jinja', { 'errors': errors, 'success': succ })	
+
+@login_required
 def get_report(request, date):
-	events = Event.objects.filter(processed=date)
+	nr = get_object_or_404(NorlonnReport, date=date)
 	linjer = []
 	personshift = {}
 
-	for e in events:
-		shift = Shift.objects.filter(event=e)
-		for s in shift:
-			if s.worker.norlonn_number in personshift:
-				personshift[s.worker.norlonn_number].append(s)
-			else:
-				personshift[s.worker.norlonn_number] = [s, ]
-	print(str(personshift))
+	shift = Shift.objects.filter(norlonn_report=nr)
+	for s in shift:
+		if s.worker.norlonn_number in personshift:
+			personshift[s.worker.norlonn_number].append(s)
+		else:
+			personshift[s.worker.norlonn_number] = [s, ]
+	
 	for (nl,ss) in personshift.items():
 		total = Decimal(0)
 		
