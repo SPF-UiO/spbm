caf194a31358b0783c332ba0507630966c88b5ef
Thor K. H <thor@roht.no>
2016-07-17 01:09:17 +0200

    Added names for urls, see in context of base.jinja

--- invoices/urls.py
+++ invoices/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 		url(r'^$', 'invoices.views.redirect_society', name="invoices"),
-		url(r'^all/$', 'invoices.views.invoices_all'),
-		url(r'^list/$', 'invoices.views.invoices_list'),
+		url(r'^all/$', 'invoices.views.invoices_all', name="invoices-all"),
+		url(r'^list/$', 'invoices.views.invoices_list', name="invoices-list"),
 		url(r'^(?P<society_name>[A-Za-z]+)/$', 'invoices.views.index'),
 		url(r'^(?P<society_name>[A-Za-z]+)/(?P<date>\d{4}-\d{2}-\d{2}).pdf$', 'invoices.views.generate'),
 )
