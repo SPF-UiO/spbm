9401b447826e7205e1bdb8236c17afe8efb34019
Thor K. H <thor@roht.no>
2016-07-17 00:48:51 +0200

    Updated configuration for TEMPLATES to work on dev

--- spf_web/settings.py
+++ spf_web/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
@@ -30,17 +31,19 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-                'django_jinja',
-                'society',
-                'workers',
-                'events',
-                'accounts',
-                'invoices',
-                'norlonn',
+    'django_jinja',
+    'widget_tweaks',
+    'society',
+    'workers',
+    'events',
+    'accounts',
+    'invoices',
+    'norlonn',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,7 +56,7 @@ ROOT_URLCONF = 'spf_web.urls'
 WSGI_APPLICATION = 'spf_web.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -63,28 +66,23 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+LANGUAGE_CODE = 'en-uk'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
-#TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_files")
 MEDIA_URL = "/media_files/"
-
-#TEMPLATE_LOADERS = (
-#        'django_jinja.loaders.FileSystemLoader',
-#        'django_jinja.loaders.AppLoader',
-#)
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale")
+]
 
 TEMPLATES = [
     {
@@ -95,6 +93,9 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'match_extension': '.jinja',
+            'filters': {
+                'attr': 'widget_tweaks.templatetags.widget_tweaks.set_attr'
+            },
             'context_processors': [
                 # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
                 # list if you haven't customized them:
@@ -120,14 +121,14 @@ TEMPLATES = [
                 # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
                 # list if you haven't customized them:
                 'django.contrib.auth.context_processors.auth',
-#                'django.template.context_processors.debug',
-#                'django.template.context_processors.i18n',
-#                'django.template.context_processors.media',
-#                'django.template.context_processors.static',
-#                'django.template.context_processors.tz',
-#                'django.contrib.messages.context_processors.messages',
-#                # Added from old SPF
-#                'django.template.context_processors.request'
+                #                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                #                'django.template.context_processors.media',
+                #                'django.template.context_processors.static',
+                #                'django.template.context_processors.tz',
+                #                'django.contrib.messages.context_processors.messages',
+                #                # Added from old SPF
+                #                'django.template.context_processors.request'
             ],
             'loaders': [
                 # insert your TEMPLATE_LOADERS here
@@ -138,10 +139,9 @@ TEMPLATES = [
     },
 ]
 
-
 AUTHENTICATION_BACKENDS = ('accounts.backend.SPFBackend', 'django.contrib.auth.backends.ModelBackend',)
 
 try:
-        from spf_web.local_settings import *
+    from spf_web.local_settings import *
 except ImportError as e:
-        pass
+    pass
