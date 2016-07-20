3fab932f94c261f00829b2da5ced3c3bc9fe49d3
Thor K. H <thor@roht.no>
2016-07-20 13:10:39 +0200

    Added named URL-reference for logging out.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.jinja'}),
-                       url(r'^logout/$', 'django.contrib.auth.views.logout'),
+                       url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
                        url(r'^password_change/$', 'django.contrib.auth.views.password_change'),
                        url(r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
                        )
