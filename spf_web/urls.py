3e07b7f23455ec562a03256185d67b55537f1797
Thor K. H <thor@roht.no>
2016-12-22 15:57:54 +0100

    Remved "socurls" in prep for socity as main fallback.
    With society having the last URL pattern, it'll have root and then treat its own URLs from there.
    Alternatively, the same pattern will continue, but socurls is out.

--- spf_web/urls.py
+++ spf_web/urls.py
@@ -5,15 +5,6 @@ from django.contrib import admin
 
 from spf_web.apps.society.views.views_overview import redirect_society as standard_index
 
-socurls = patterns('',
-                   url(r'^$', include('spf_web.apps.society.urls')),
-                   url(r'^society/', include('spf_web.apps.society.urls')),
-                   url(r'^workers/', include('spf_web.apps.society.urls')),
-                   url(r'^events/', include('spf_web.apps.events.urls')),
-                   url(r'^invoices/', include('spf_web.apps.invoices.urls')),
-                   url(r'^norlonn/', include('spf_web.apps.norlonn.urls')),
-                   )
-
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^accounts/', include('spf_web.apps.accounts.urls')),
@@ -22,6 +13,5 @@ urlpatterns = patterns('',
                        url(r'^events/', include('spf_web.apps.events.urls')),
                        url(r'^invoices/', include('spf_web.apps.invoices.urls')),
                        url(r'^norlonn/', include('spf_web.apps.norlonn.urls')),
-                       # url(r'^(?P<society_name>[A-Za-z]+)/', include(socurls)),
                        url(r'^$', standard_index),
                        ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
