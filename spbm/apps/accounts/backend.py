d77b6ffbcfcbf3cb3624dcfd036a1ba8b7c90b10
Thor K. H <thor@roht.no>
2017-01-08 13:44:16 +0000

    Attempting to describe why there's an SPFBackend for authentication.

--- spbm/apps/accounts/backend.py
+++ spbm/apps/accounts/backend.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 
 
 class SPFBackend(object):
+    """
+    Custom authentication backend for some SPF requirements, so to speak.
+
+    Per conversation with Aleksi, Jan 7th 2017, over the DARPANET, the following two aspects were the
+    reason for the custom authentication backend, and is as such what it does. Mainly.
+
+        1. Allows for case-insensitive usernames.
+            - Whether or not a custom backend is necessary with Django 1.10 for this, I haven't checked.
+        2. Mitigates username timing attacks.
+            - Same applies as above. By always computing a hash-function, you mitigate (somewhat?) against
+              timing attacks to brute-force a correct and valid username.
+    """
     def authenticate(self, username=None, password=None):
         try:
             user = User.objects.get(username__iexact=username)
