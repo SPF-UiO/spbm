e89a271dc3f896d10a6026449548eec2f8419cf0
Thor K. H <thor@roht.no>
2017-02-09 14:03:38 +0100

    Added verbosity and help_text for SpfUser

--- spbm/apps/accounts/models.py
+++ spbm/apps/accounts/models.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 from django.db import models
 
 
 class SpfUser(models.Model):
+    class Meta:
+        verbose_name = _('SPF-user')
+        verbose_name_plural = _('SPF-users')
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    society = models.ForeignKey('society.Society', null=True, on_delete=models.SET_NULL)
+    society = models.ForeignKey('society.Society', verbose_name=_('Society'), null=True, on_delete=models.SET_NULL,
+                                help_text=_('The society within SPF that the user has as a primary connection.'))
+
+    def __str__(self):
+        return ugettext("SPF-member connected to {society}".format(society=self.society))
