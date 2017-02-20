ca97fad3d6a700f5c38be99b75a6ce678129f17c
Thor K. H <thor@roht.no>
2017-02-20 01:15:54 +0100

    Updated runner script to actually work

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -3,16 +3,19 @@
 # Correct virtualenv for execution
 source .virtualenv/bin/activate
 
+# Source path -- simple and to the point
+SRC=$HOME/src
+
 # Execute uwsgi for hosting/running the server
-uwsgi --chdir=~/src/spbm/ \
---module spbm.wsgi:application \
---master --pidfile=/tmp/spf-uwsgi.pid \
---socket=127.0.0.1:8435 \
---processes=1 \
---harakiri=20 \
---max-requests=5000 \
---vacuum \
---home=~/src/.virtualenv/ \
---daemonize=~/logs/website.log \
---touch-reload=~/src/spbm/wsgi.py
+uwsgi --chdir=$SRC/ \
+    --module spbm.wsgi:application \
+    --master --pidfile=/tmp/spf-uwsgi.pid \
+    --socket=127.0.0.1:8435 \
+    --processes=1 \
+    --harakiri=20 \
+    --max-requests=5000 \
+    --vacuum \
+    --home=$SRC/.virtualenv/ \
+    --daemonize=$HOME/logs/website.log \
+    --touch-reload=$SRC/spbm/wsgi.py
 
