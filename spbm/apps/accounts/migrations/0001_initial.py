dd5043d5ef012ea47ec14d5aafe295023380f71b
Thor K. H <thor@roht.no>
2017-03-13 15:23:03 +0100

    Squashing events, invoices and workers
    Creates a big squashed migration, and modifies the initial account
    migration to point to it.
    Must be applied at all instances before it can be changed into a normal
    migration.

--- spbm/apps/accounts/migrations/0001_initial.py
+++ spbm/apps/accounts/migrations/0001_initial.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
 import django.db.models.deletion
 from django.conf import settings
+from django.db import models, migrations
+
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('society', '0001_initial'),
+        ('society', '0001_squashed_app_merges'),
     ]
 
     operations = [
@@ -18,7 +18,8 @@ class Migration(migrations.Migration):
             name='SpfUser',
             fields=[
                 ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('society', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, null=True, to='society.Society')),
+                ('society',
+                 models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, null=True, to='society.Society')),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
