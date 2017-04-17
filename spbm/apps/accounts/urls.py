c1fbf33dee366a301f6364a113dbb821c30a0172
Thor K. H <thor@roht.no>
2017-04-17 13:54:57 +0100

    Django 1.11 support, with updated dependencies.

--- spbm/apps/accounts/urls.py
+++ spbm/apps/accounts/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^login/$', views.login, {'template_name': 'accounts/login.jinja'}),
-    url(r'^logout/$', views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^password_change/$', views.password_change),
-    url(r'^password_reset/$', views.password_reset),
+    url(r'^login/$', views.LoginView.as_view(template_name='accounts/login.jinja')),
+    url(r'^logout/$', views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'^password_change/$', views.PasswordChangeView.as_view()),
+    url(r'^password_reset/$', views.PasswordResetView.as_view()),
 ]
