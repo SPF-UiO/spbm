e8526f6779649af8ac2cb92570a3c97fd6a09bc3
Thor K. H <thor@roht.no>
2017-07-17 14:13:27 +0200

    Returning to handle_no_permission()
    Should work fine, seeing as we have raise_exception to True per default.

--- spbm/helpers/mixins.py
+++ spbm/helpers/mixins.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.views import redirect_to_login
-from django.core.exceptions import PermissionDenied
 
 
 class LoginAndPermissionRequiredMixin(PermissionRequiredMixin):
     """
     Custom mixin to merge login and permission required.
     """
+
+    raise_exception = True
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
@@ -15,5 +16,6 @@ class LoginAndPermissionRequiredMixin(PermissionRequiredMixin):
                                      )
         if not self.has_permission():
             # We could also use "return self.handle_no_permission()" here
-            raise PermissionDenied(self.get_permission_denied_message())
+            # raise PermissionDenied(self.get_permission_denied_message())
+            return self.handle_no_permission()
         return super(LoginAndPermissionRequiredMixin, self).dispatch(request, *args, **kwargs)
