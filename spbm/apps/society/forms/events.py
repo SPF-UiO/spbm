c2bc57d067adf5041c4ebb80b0d641b8668e8699
Thor K. H <thor@roht.no>
2017-07-17 14:12:42 +0200

    Added classes and step values to event forms

--- spbm/apps/society/forms/events.py
+++ spbm/apps/society/forms/events.py
@@ -1,30 +1,32 @@
-from django.forms import DateInput
-from django.forms import ModelForm
+from django import forms
 
 from spbm.apps.society.models import Worker, Event, Shift
 
 
-class MyDateInput(DateInput):
+class DateInput(forms.DateInput):
     input_type = 'date'
 
 
-class EventForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)
-        self.fields['date'].widget = MyDateInput(attrs={'class': 'date'})
-
+class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'date']
+        widgets = {
+            'date': DateInput(attrs={'class': 'date'})
+        }
 
 
 def make_shift_base(society):
     """ Takes a society and returns a Shift form with initial data populated based of society defaults """
 
-    class ShiftForm(ModelForm):
+    class ShiftForm(forms.ModelForm):
         class Meta:
             model = Shift
             exclude = ['norlonn_report']
+            widgets = {
+                'wage': forms.NumberInput(attrs={'step': 1}),
+                'hours': forms.NumberInput(attrs={'step': '0.25'})
+            }
 
         def __init__(self, *args, **kwargs):
             super(ShiftForm, self).__init__(*args, initial={'wage': society.default_wage}, **kwargs)
