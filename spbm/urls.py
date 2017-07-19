b8d541aaf66fcd5b0776badde8f9c15815b2d7e5
Thor K. H <thor@roht.no>
2017-07-19 14:38:37 +0200

    Added Django Debug Toolbar

--- spbm/urls.py
+++ spbm/urls.py
@@ -7,8 +7,13 @@ from spbm.apps.society.views import PermissionDeniedView
 
 handler403 = PermissionDeniedView.as_view()
 
-urlpatterns = [
-                  url(r'^admin/', admin.site.urls),
-                  url(r'^accounts/', include('spbm.apps.accounts.urls')),
-                  url(r'^', include('spbm.apps.society.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [url(r'^admin/', admin.site.urls),
+               url(r'^accounts/', include('spbm.apps.accounts.urls')),
+               url(r'^', include('spbm.apps.society.urls')),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Django Debug Toolbar, and other debug-related URLconfs
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls)),
+                   ] + urlpatterns
