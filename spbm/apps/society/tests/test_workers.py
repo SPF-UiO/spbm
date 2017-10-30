0981e7c0ed287e40df7ea73c6a38943f2c5a8c03
Thor K. H <thor@roht.no>
2017-10-30 20:24:50 +0100

    Improved tests for workers that creates normal, dupes and more
    - Tries to create a minimal worker
    - Tries to create a full one
    - Tries to create worker with invalid national ID and bank account
    - Tries to create worker with duplicate Norlønn number and national ID
    
    The latter will continue to work till we've migrated and removed old
    dupes from the current production set.

--- spbm/apps/society/tests/test_workers.py
+++ spbm/apps/society/tests/test_workers.py
@@ -4,43 +4,108 @@ from django.urls import reverse
 
 from spbm.apps.accounts.models import SpfUser
 from spbm.apps.society.models import Society, Worker
-from . import test_fixtures
+from . import test_fixtures, SPFTest
 
 
-class WorkerTests(TestCase):
+class WorkerTests(SPFTest, TestCase):
     fixtures = test_fixtures
-
-    def setUp(self):
-        # Logging into the site as a test in itself can be somewhere else. This doesn't need it.
-        self.user = User(username='cyb')
-        self.user.save()
-        self.spf_user = SpfUser(user=self.user, society=Society.objects.get(pk=2))
-        self.spf_user.save()
-        self.client.force_login(self.user)
 
     @classmethod
     def setUpTestData(cls):
         cls.test = True
         cls.society_name = 'CYB'
+        cls.last_fixtures_worker = Worker.objects.last()
 
-    def test_create_worker(self):
-        # TODO: finish test
+    def setUp(self):
+        # Logging into the site as a test in itself can be somewhere else. This doesn't need it.
+        self.user = User(username='cyb')
+        self.user.save()
+        self.spf_user = SpfUser(user=self.user, society=Society.objects.get(shortname=self.society_name))
+        self.spf_user.save()
+        self.client.force_login(self.user)
+
+    def test_create_minimum_worker(self):
+        """
+        Creates the smallest worker possible.
+        """
+        worker = {
+            'name': 'Ola Normann',
+            'address': 'Adressegata 1, 0123 Oslo',
+        }
+        # Confirm that permissions stop us
+        self.assertEqual(self.client.post(reverse('worker-create', args=[self.society_name]), data=worker).status_code,
+                         403)
+        # Confirm that adding permissions actually works
+        self.user.user_permissions.add(Permission.objects.get(codename='add_worker'))
+        post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        self.assertNotEqual(self.last_fixtures_worker, Worker.objects.last())
+        self.assertEqual(post.status_code, 200)
+
+    def test_create_full_worker(self):
+        """
+        Creates a worker with all the fields.
+        """
         worker = {
             'name': 'Ola Normann',
             'norlonn_number': '100',
+            'address': 'Adressegata 1, 0123 Oslo',
+            'account_no': '1100.12.34562',
             'person_id': '01020394311',
-
         }
-        self.assertEqual(self.client.post(reverse('worker-create', args=[self.society_name]), data=worker).status_code,
-                         403)
         self.user.user_permissions.add(Permission.objects.get(codename='add_worker'))
-        last_worker = Worker.objects.last()
-        post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker)
-        self.assertNotEqual(last_worker, Worker.objects.last())
+        post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        self.assertNotEqual(self.last_fixtures_worker, Worker.objects.last())
+        self.assertEqual(post.status_code, 200)
+
+    def test_create_worker_invalid_account_number(self):
+        """
+        Tries to create a worker with an invalid account number.
+        """
+        worker = {
+            'name': 'Ola Normann',
+            'address': 'Adressegata 1, 0123 Oslo',
+            # off by one, so to speak
+            'account_no': '1100.12.34563',
+        }
+        self.user.user_permissions.add(Permission.objects.get(codename='add_worker'))
+        post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        self.assertEqual(self.last_fixtures_worker, Worker.objects.last())
+        self.assertFormError(post, "form", "account_no",
+                             "Invalid control digit. Enter a valid Norwegian bank account number.")
         self.assertEqual(post.status_code, 200)
 
     def test_create_worker_duplicate_norlonn(self):
-        pass
+        """
+        Tries to create a worker with a duplicate national ID.
+        """
+        self.user.user_permissions.add(Permission.objects.get(codename='add_worker'))
+        worker = {
+            'name': 'Ola Normann',
+            'address': 'Adressegata 1, 0123 Oslo',
+            'norlonn_number': '999',
+        }
+        first_post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        first_added_worker = Worker.objects.last()
+        second_post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        self.assertEqual(first_added_worker, Worker.objects.last(),
+                         "Worker created despite having a duplicate wage personnel number.")
+        self.assertEqual(first_post.status_code, 200)
+        self.assertEqual(second_post.status_code, 200)
 
     def test_create_worker_duplicate_national_id(self):
-        pass
+        """
+        Tries to create a worker with a duplicate national ID.
+        """
+        self.user.user_permissions.add(Permission.objects.get(codename='add_worker'))
+        worker = {
+            'name': 'Ola Normann',
+            'address': 'Adressegata 1, 0123 Oslo',
+            'person_id': '01020332596',
+        }
+        first_post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        first_added_worker = Worker.objects.last()
+        second_post = self.client.post(reverse('worker-create', args=[self.society_name]), data=worker, follow=True)
+        second_added_worker = Worker.objects.last()
+        self.assertEqual(first_added_worker, second_added_worker,
+                         "Worker created despite having a duplicate national ID.")
+        self.assertEqual(first_post.status_code, 200)
