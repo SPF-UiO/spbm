3d9d8b99696694bc0d5966a00309fb97cc0c2174
Thor K. H <thor@roht.no>
2018-06-17 19:10:48 +0200

    New overview screen!
    Not very performant, but alright given its non-dynamic nature.

--- spbm/apps/society/views/overview.py
+++ spbm/apps/society/views/overview.py
@@ -1,10 +1,13 @@
+from decimal import Decimal
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Sum, F
+from django.db.models import Sum, F, Min
 from django.db import models
+from django.urls import reverse
 from django.views.generic import TemplateView
 from django.utils.translation import ugettext as _
 
-from spbm.apps.society.models import Event, Invoice
+from spbm.apps.society.models import Event, Invoice, Worker
 from spbm.helpers.auth import user_society
 
 
@@ -20,50 +23,81 @@ class Overview(LoginRequiredMixin, TemplateView):
 
         context.update({
             'society': user_society(self.request),
-            'dashboard': get_dashboard_information()
+            'dashboard': {
+                'general': self.get_general_status_blocks(),
+                'actionable': self.get_actionable_blocks()
+            }
         })
 
         return context
 
+    def get_general_status_blocks(self):
+        # FIXME: There *might* not be two invoices, you know.
+        invoices = Invoice.objects.all().order_by('-period')
+        society_invoices = invoices.filter(society=user_society(self.request))
+        last_period = invoices[0]
 
-def get_dashboard_information():
-    invoices = Invoice.objects.all().order_by('-period')
-    this_period = invoices[0]
-    last_period = invoices[1]
+        events = Event.objects.all().select_related('shifts__worker')
+        events_last_period = events.filter(invoice=last_period)
+        events_this_period = events.filter(invoice=None)
 
-    events = Event.objects.all()
-    events_last_period = events.filter(invoice=last_period)
-    events_this_period = events.filter(invoice=this_period)
+        wages_this_period = (events_this_period
+                             .aggregate(wages=Sum(F('shifts__hours') * F('shifts__wage'),
+                                                  output_field=models.DecimalField(decimal_places=2))))['wages']
+        wages_last_period = (events_last_period
+                             .aggregate(wages=Sum(F('shifts__hours') * F('shifts__wage'),
+                                                  output_field=models.DecimalField(decimal_places=2))))['wages']
 
-    wages_this_period = (events.filter(invoice=this_period)
-        .aggregate(wages=Sum(F('shifts__hours') * F('shifts__wage'),
-                             output_field=models.DecimalField(decimal_places=2))))['wages']
-    wages_last_period = (events.filter(invoice=last_period)
-        .aggregate(wages=Sum(F('shifts__hours') * F('shifts__wage'),
-                             output_field=models.DecimalField(decimal_places=2))))['wages']
+        # workers_this_period = (events.filter(invoice=last_period).select_related('shifts__worker')
+        #                        .distinct('shifts__worker')).count()
+        workers_this_period = (events_this_period
+                               .values('shifts__worker')).annotate(shifts=Min('shifts__worker')).count()
+        # workers_last_period = (events.filter(invoice=last_period).select_related('shifts__worker')
+        #                        .distinct('shifts__worker')).count()
+        workers_last_period = (events_last_period
+                               .values('shifts__worker')).annotate(shifts=Min('shifts__worker')).count()
 
-    workers_this_period = (events.filter(invoice=last_period).select_related('shifts__worker')
-        .distinct('shifts__worker')).count()
-    workers_last_period = (events.filter(invoice=last_period).select_related('shifts__worker')
-        .distinct('shifts__worker')).count()
+        return [
+            {'title': _("Events"),
+             'kpi': events_this_period.count(),
+             'change': change(events_last_period.count(), events_this_period.count()),
+             'time': _("last period"),
+             'url': reverse('events')},
 
-    return [
-        {'title': _("Events"),
-         'kpi': events_this_period.count(),
-         'change': change(events_last_period.count(), events_this_period.count()),
-         'time': _("last period")},
+            {'title': _("Wages"),
+             'kpi': wages_this_period,
+             'change': change(wages_last_period, wages_this_period),
+             'time': _("last period"),
+             'url': reverse('wages')},
 
-        {'title': _("Wages"),
-         'kpi': wages_this_period,
-         'change': change(wages_last_period, wages_this_period),
-         'time': _("last period")},
+            {'title': _("Workers"),
+             'kpi': workers_this_period,
+             'change': change(workers_last_period, workers_this_period),
+             'time': _("last period"),
+             'url': reverse('workers')},
+        ]
 
-        {'title': _("Payroll Workers"),
-         'kpi': workers_this_period,
-         'change': change(workers_last_period, workers_this_period),
-         'time': _("last term")},
-    ]
+    def get_actionable_blocks(self):
+        blocked_wages = (Event.objects
+                         .filter(society=user_society(self.request), invoice__paid=False)
+                         .aggregate(wages=Sum(F('shifts__hours') * F('shifts__wage'),
+                                              output_field=models.DecimalField(decimal_places=2))))['wages']
+        missing_ids = (Worker.objects
+                       .filter(employment__society=user_society(self.request), norlonn_number__isnull=True).count())
+
+        return [
+            {'title': _("Your Blocked Wages"),
+             'kpi': blocked_wages if blocked_wages else None,
+             'detail': _("Wages blocked by unpaid invoices"),
+             'url': reverse('invoices'),
+             'alert': 'success'},
+
+            {'title': _("Your Missing Norlønn-IDs"),
+             'kpi': missing_ids if missing_ids else None,
+             'detail': _("Workers whose pay will not be exported"),
+             'url': reverse('workers')}
+        ]
 
 
-def change(before, after):
-    return ((before / after) - 1) * 100.00
+def change(before: Decimal, after: Decimal):
+    return Decimal(((before / after) - 1)) * Decimal(100.00)
