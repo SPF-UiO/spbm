1f3a91dd0a967754ae8440f3cd2f72643bf93270
Thor K. H <thor@roht.no>
2018-06-17 19:19:50 +0200

    Fix overview index name in tests and replace with sub-tests

--- spbm/apps/society/tests/test_generic.py
+++ spbm/apps/society/tests/test_generic.py
@@ -3,12 +3,13 @@ from django.urls import reverse
 
 
 class URLTests(SimpleTestCase):
+    HTTP_OK = 200
 
     def test_section_url_names(self):
-        self.assertEqual(self.client.get(reverse('index'), follow=True).status_code, 200)
-        self.assertEqual(self.client.get(reverse('workers'), follow=True).status_code, 200)
-        self.assertEqual(self.client.get(reverse('events'), follow=True).status_code, 200)
-        self.assertEqual(self.client.get(reverse('invoices'), follow=True).status_code, 200)
-        self.assertEqual(self.client.get(reverse('wages'), follow=True).status_code, 200)
-
-
+        """
+        Test that we can access the normal sections by their section names, and that they work.
+        """
+        views = ['overview', 'workers', 'events', 'invoices', 'wages', 'admin:index']
+        for view in views:
+            with self.subTest(msg="testing {view}".format(view=view)):
+                self.assertEqual(self.client.get(reverse(view), follow=True).status_code, self.HTTP_OK)
