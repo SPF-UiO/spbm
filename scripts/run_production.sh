af89ad86cf82b5ca9bb0e9e6c9121501f592c59b
Maximiliano Horta <maximilianohorta94@gmail.com>
2019-02-19 19:41:49 +0100

    Add start.sh, use Debian in Dockerfile
    Thanks to Adrian for suggesting Debian based Docker container.

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -ex
 
 # Correct virtualenv for execution
-source .virtualenv/bin/activate
+#source .virtualenv/bin/activate
 
 # Source path -- simple and to the point
-SRC=$HOME/src
+SRC=.
 
 # Execute uwsgi for hosting/running the server
-uwsgi --chdir=$SRC/ \
-    --module spbm.wsgi:application \
+uwsgi --module spbm.wsgi:application \
     --master --pidfile=/tmp/spf-uwsgi.pid \
     --socket=127.0.0.1:8435 \
     --processes=1 \
     --harakiri=20 \
     --max-requests=5000 \
     --vacuum \
-    --home=$SRC/.virtualenv/ \
-    --daemonize=$HOME/logs/website.log \
+    #--home=$SRC/.virtualenv/ \
+    --daemonize=$SRC/logs/website.log \
     --touch-reload=$SRC/spbm/wsgi.py
 
