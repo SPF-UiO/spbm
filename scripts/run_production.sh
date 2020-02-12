93ebacfbe8664139075d7107eab4927c8f8e250d
Thor K. H <thor@roht.no>
2020-02-12 13:29:16 +0100

    Add entrypoint.sh

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -1,20 +1,7 @@
 #!/bin/bash -ex
 
-# Correct virtualenv for execution
-#source .virtualenv/bin/activate
-
-# Source path -- simple and to the point
-SRC=.
-
-# Execute uwsgi for hosting/running the server
-uwsgi --module spbm.wsgi:application \
-    --master --pidfile=/tmp/spf-uwsgi.pid \
-    --socket=127.0.0.1:8435 \
-    --processes=1 \
-    --harakiri=20 \
-    --max-requests=5000 \
-    --vacuum \
-    #--home=$SRC/.virtualenv/ \
-    --daemonize=$SRC/logs/website.log \
-    --touch-reload=$SRC/spbm/wsgi.py
+# TODO: Execute the default Compose
+# - Retain old image
+# - Get new image?
+# - docker-compose up?
 
