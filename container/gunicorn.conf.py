2e49f46bfd3bd28c8c2bb2abbb6879d17d2a40d1
Thor K. H <thor@roht.no>
2020-02-14 00:35:54 +0100

    Improve Gunicorn configuration, live reloads and use run with depends_on

--- container/gunicorn.conf.py
+++ container/gunicorn.conf.py
@@ -3,8 +3,13 @@ Gunicorn configuration file for loading up SPBM.
 """
 import os
 import multiprocessing
+from glob import glob
+
+debug = int(os.environ.get("SPBM_DEBUG", default=0))
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "meinheld.gmeinheld.MeinheldWorker"
-reload = True if os.environ.get("SPBM_DEBUG", default=0) else False
+workers = multiprocessing.cpu_count() + 1
+worker_class = "meinheld.gmeinheld.MeinheldWorker" if not debug else "sync"
+reload_engine = "inotify"
+reload = True if debug else False
+reload_extra_files = glob("../**/*.py", recursive=True) if debug else []
