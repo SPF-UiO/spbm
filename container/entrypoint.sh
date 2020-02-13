fe1567274cc3905e88171adb56b9f5c5511c1e3e
Thor K. H <thor@roht.no>
2020-02-13 16:59:14 +0100

    Wait for PSQL if necessary

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+if [[ "$SPBM_DB_ENGINE" == "django.db.backends.postgresql" ]]; then
+	./container/wait-for-it.sh psql:5432 -t 20
+fi
+
 if [[ ! "$DEBUG" ]]; then
   echo "Migrating database due to SPBM_DEBUG being false"
   python manage.py showmigrations --plan
