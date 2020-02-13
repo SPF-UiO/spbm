e0ee5778d28e1a1d98171e8729e8febd7b6cd4ce
Thor K. H <thor@roht.no>
2020-02-13 23:27:24 +0100

    Revert premature edit of SPBM_DEBUG -> DEBUG

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -4,7 +4,7 @@ if [[ "$SPBM_DB_ENGINE" == "django.db.backends.postgresql" ]]; then
   ./container/wait-for-it.sh psql:5432 -t 20
 fi
 
-if [[ ! "$DEBUG" ]]; then
+if [[ ! "$SPBM_DEBUG" ]]; then
   echo "Migrating database due to SPBM_DEBUG being false"
   python manage.py showmigrations --plan
   python manage.py migrate
