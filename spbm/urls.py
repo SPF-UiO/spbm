3e3e8bed2490a9565ce68b8e8976a35abe9570e7
Thor K. Høgås <thor@roht.no>
2022-06-12 19:10:27 +0200

    fix(tests): debug toolbar works under testing
    Uses settings.DEBUG as the test setup will change settings.DEBUG to
    False, whereas our DEBUG in settings.py will not by default do that.
    
    Furthermore, always adds the debug URLs to the application's URL
    patterns, resolving the KeyError that could occur when djdt is partially
    loaded, but not fully, when running tests with SPBM_DEBUG set to True in
    the parent environment.

--- spbm/urls.py
+++ spbm/urls.py
@@ -2,23 +2,16 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+import debug_toolbar
 
 from spbm.apps.society.views import PermissionDeniedView
 
-"""
-This 
-"""
-
 handler403 = PermissionDeniedView.as_view()
 
-urlpatterns = [url(r'^admin/', admin.site.urls),
-               url(r'^accounts/', include('spbm.apps.accounts.urls')),
-               url(r'^', include('spbm.apps.society.urls')),
-               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Django Debug Toolbar, and other debug-related URLconfs (should they be added)
-if settings.DEBUG:  # pragma: no cover
-    import debug_toolbar
-
-    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls)),
-                   ] + urlpatterns
+urlpatterns = [
+    # Django Debug Toolbar always included to make testing less fragile
+    url(r"^__debug__/", include(debug_toolbar.urls)),
+    url(r"^admin/", admin.site.urls),
+    url(r"^accounts/", include("spbm.apps.accounts.urls")),
+    url(r"^", include("spbm.apps.society.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
